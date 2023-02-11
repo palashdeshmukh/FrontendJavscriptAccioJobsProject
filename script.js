822771c144c0ee678d4116cef8438ec53ef60345
Palash Deshmukh <115765112+palashdeshmukh@users.noreply.github.com>
2023-02-11 12:26:01 -0500

    Update script.js
    first commit

--- script.js
+++ script.js
@@ -7,21 +7,42 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+ arr.map(({id,name,age, marks})=>{
+    if(marks >= 50){
+      console.log(arr[id-1]);
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(({id,name,age, marks})=>{
+    if(marks >= 50){
+      console.log(arr[id-1]);
+    }
+    });
+  
 }
 
 function addData() {
   //Write your code here, just console.log
+   const added = {id : 4, name : "susan", age : "20", marks :45 };
+  arr.push(added);
+  console.log(arr);
+   
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+   console.log(arr.filter(f=>f.marks > 50));
+  
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const arr1 = [
+  { id: 4, name: "john", age: "17", marks: 67 },
+  { id: 5, name: "nick", age: "22", marks: 90 },
+  { id: 6, name: "james", age: "18", marks: 45 },]
+  arr = arr.concat(arr1);
+  console.log(arr);
+  
 }
